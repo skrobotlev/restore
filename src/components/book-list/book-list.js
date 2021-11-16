6fe752bd2ecd01df4a1814199bb7905f1798ab67
usr109283jsmzs812 <83765120+usr149928321@users.noreply.github.com>
2021-11-16 12:54:38 +0300

    done16ComponentContainer

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,21 @@ import ErrorIndicator from '../error-indicator';
 
 import './book-list.css'
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+    return (
+        <ul className="book-list">
+            {
+                books.map((book) => {
+                    return (
+                        <li key={book.id}><BookListItem book={book} /></li>
+                    )
+                })
+            }
+        </ul>
+    );
+}
+
+class BookListContainer extends Component {
 
     componentDidMount() {
         this.props.fetchBooks();
@@ -37,19 +51,10 @@ class BookList extends Component {
         if (error) {
             return <ErrorIndicator />
         }
-        return (
-            <ul className="book-list">
-                {
-                    books.map((book) => {
-                        return (
-                            <li key={book.id}><BookListItem book={book} /></li>
-                        )
-                    })
-                }
-            </ul>
-        );
+        return <BookList books={books} />
     }
 }
+
 // не до конца понимаю MapStates
 const mapStateToProps = ({ books, loading, error }) => {
     return { books, loading, error }
@@ -90,4 +95,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 
 export default compose(
     withBookstoreService(),
-    connect(mapStateToProps, mapDispatchToProps))(BookList);
+    connect(mapStateToProps, mapDispatchToProps))(BookListContainer);
