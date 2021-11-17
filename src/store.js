3ea3730a7ae6bb797e5024a3206ca6444c7c29a1
usr109283jsmzs812 <83765120+usr149928321@users.noreply.github.com>
2021-11-17 20:01:53 +0300

    done23middleware

--- src/store.js
+++ src/store.js
@@ -1,32 +1,50 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
 
-const logEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        console.log(action.type);
-        return originalDispatch(action)
+const logMiddleWare = ({ getState }) => (dispatch) => (action) => {
+    console.log(action.type, getState());
+    return dispatch(action);
+}
+// const logMiddleWare = (store) => (dispatch) => (action) => {
+//     console.log(action.type, store.getState());
+//     return dispatch(action);
+// }
+// ПИШЕТСЯ ЛИБО NEXT ЛИБО DISPATCH
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
     }
-    return store;
-};
-
-const stringEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-
-        if (typeof action === 'string') {
-            return originalDispatch({
-                type: action
-            })
-        }
-        originalDispatch(action)
-    }
-    return store;
+    return next(action);
 }
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+// const logEnhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         console.log(action.type);
+//         return originalDispatch(action)
+//     }
+//     return store;
+// };
+
+// const stringEnhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+
+//         if (typeof action === 'string') {
+//             return originalDispatch({
+//                 type: action
+//             })
+//         }
+//         originalDispatch(action)
+//     }
+//     return store;
+// }
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleWare));
 
 // MONKEY PATCHING
 // const originalDispatch = store.dispatch;
